74fc493f23112dfa866d07fa7527616081b9fa7d
Luis Aguila <aguilaluis22@gmail.com>
2022-03-02 22:12:10 -0700

    v 0.0.1
    Se adjunta la libreria de graficas y se creó un menú que direcciona a las distintas páginas para la visualiación del Indicador, consulta y contacto además de la página de captura que ya se encontraba anteriormente.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var viajes_totales;
 var precision;
 var meta = 95;
 var dia = 0;
-var barra;
 
 function ejecutar() {
     viajes_tiempo = document.getElementById("dato1").value;
@@ -13,7 +12,6 @@ function ejecutar() {
     viajes_totales = parseInt(viajes_tiempo) + parseInt(viajes_tarde);
     precision = (viajes_tiempo / viajes_totales)*100;
     dia = dia + 1;
-    barra = "|".repeat(precision);
 
     var table = document.getElementById("myTable");
     var row = table.insertRow(-1);
@@ -22,12 +20,42 @@ function ejecutar() {
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
-    var cell6 = row.insertCell(5);
 
     cell1.innerHTML = dia;
     cell2.innerHTML = viajes_tiempo;
     cell3.innerHTML = viajes_tarde;
     cell4.innerHTML = viajes_totales;
     cell5.innerHTML = precision.toFixed(2);
-    cell6.innerHTML = barra;
 }
+
+// Obtener una referencia al elemento canvas del DOM
+const $grafica = document.querySelector("#grafica");
+// Las etiquetas son las que van en el eje X. 
+const etiquetas = ["Enero", "Febrero", "Marzo", "Abril"]
+// Podemos tener varios conjuntos de datos. Comencemos con uno
+const datosVentas2020 = {
+    label: "Ventas por mes",
+    data: [5000, 1500, 8000, 5102], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+    borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
+    borderWidth: 1,// Ancho del borde
+};
+new Chart($grafica, {
+    type: 'line',// Tipo de gráfica
+    data: {
+        labels: etiquetas,
+        datasets: [
+            datosVentas2020,
+            // Aquí más datos...
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+        },
+    }
+});
